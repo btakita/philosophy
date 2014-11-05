36864dcb521184187699b4adf74bf6c1a2f28c9d
Brian Takita <brian.takita@gmail.com>
2014-11-05 13:42:38 -0500

    Added existence-is-god.mm from https://github.com/btakita/btakita.github.io Added spiritual-work.txt

---
+++ existence-is-god.mm
@@ -0,0 +1,34 @@
+Existence is God {
+  Chapter 1 - Magical Incantations of Concepts a.k.a. Words {
+    Perspective has the power to change everything. A concept can be flipped upside down by changing your perspective. A simple experiment you can do is to hold an object (like a piece of fruit) and look at it from one direction. Then flip you head around to the other side of the fruit. Notice that it''s the same fruit, yet the appearance is different.
+
+    Be aware, you do have the memory of picking up the fruit & seeing it from the first angle and then to the second angle. This *story* gives you an awareness that there are multiple perspectives that the fruit can be viewed. You also have awareness that of seeing the fruit from both angles.
+
+    Now as an experiment, view the fruit from an angle and ignore your memory of picking up the fruit & viewing it from other angles. Experience your perspective of the fruit. What sensations do you feel? Do you feel emotions? Is there a back story behind your sensations? Be aware of the backstory, if there is one. Understand the backstory while remaining detached from it.
+
+    You have just exercised having multiple perspectives on a single object or concept. This brings us to the magical incantation of words.
+
+    The Wizard {
+      Picture this. In an ancient white tower, a wizard in white is hunched over candlelight studying texts of arcana. Since childhood, he has studied the arts of magic. He pauses to remember when such studying came to fruition. When he saved the world from dominion of evil.
+
+      On a gloomy autumn day, a dark wizard opened a portal to the realms of hell. Beyond the gate, lies the dark lord, who controls every aspect of that realm to his pleasure. The subjects toil day after day under the eyes of the taskmasters. Any deviance from the strict procedures means being stripped of freedom to live a meaningful life and then becoming indoctrinated to themselves become taskmasters. The taskmasters'' only stimulation comes from stripping away the power & freedom of their subjects. However, such pleasure is short lived as their thirst for imprisoning others is insatiable.
+
+      In the dark world, there is a saying, "You have the freedom to do what you want to do". Everyone has "freedom". Everyone can make their own decisions. Nothing impedes their free will.
+
+      Disobedience is punished with the "responsibility" to hinder yourself & others from performing life''s purposes. Failure to live up to such responsibility degrades the quality of life of the individual & imposes more "responsibility" to impede life purposes.
+
+      Now the wizard in white always saw the danger of this dark world. He toiled to bring awareness to the population against the allure of the dark world. This allure attracted mistakes, most often coming from ignorance. If he could only make the population make the "right" decisions this danger would not exist. If the population were acting perfectly, then life would be perfect. He even had contingencies against those who disobeyed. He would show them the errors of their ways and they would then educate others against the dangers of acting against the divine plan.
+
+      The White Wizard implemented his plan to lead the population towards enlightenment. Some people were called by the White Wizard & chose to follow his lead. The White Wizard spoke directly to each person through thoughts, feelings, circumstances, words, visions. The called spoke to their brethren about their experiences. Groups of people with the same spirit also merged to become souls.
+
+      He began to notice that certain spiritual personalities began to teach their personal message to the population. The devotees loved hearing the personal messages as it resonated with them. When the strong personalities passed on, the devotees created institutions to preserve & propagate the teachings of their masters. These institutions tried their best to instill the spirit & teachings of their masters. However, an institution is a life form separate from the spiritual master. Even spiritual masters are different lifeforms from themselves as they pass through life. For a soul travels on the road of time, yet evolves.
+
+
+    }
+  }
+}
+
+The Holy Grail {
+  Inner creation of your life. "You and the land are one".
+
+}
