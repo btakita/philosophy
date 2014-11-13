4b890f98e804e5fdb1340d891412d7f956d94e89
Brian Takita <brian.takita@gmail.com>
2014-11-13 03:47:44 -0500

    Added: * existence-is-god.mm & existence-is-god-book.mm * living-systems.txt * natural-processes.txt * tmp directory

--- existence-is-god.mm
+++ existence-is-god.mm
@@ -1,34 +1,218 @@
 Existence is God {
-  Chapter 1 - Magical Incantations of Concepts a.k.a. Words {
-    Perspective has the power to change everything. A concept can be flipped upside down by changing your perspective. A simple experiment you can do is to hold an object (like a piece of fruit) and look at it from one direction. Then flip you head around to the other side of the fruit. Notice that it''s the same fruit, yet the appearance is different.
+  This is an attempt to define a spiritual system with culturally true definitions. {
+    This is intended to be a living iterative document meant to interact with current culture.
+    Discussion is encouraged. The language is self-referential.
+  }
+  All definitions apply to the definitions in this document. {
+    Each section within this document is an Entity.
 
-    Be aware, you do have the memory of picking up the fruit & seeing it from the first angle and then to the second angle. This *story* gives you an awareness that there are multiple perspectives that the fruit can be viewed. You also have awareness that of seeing the fruit from both angles.
+  }
+  Document Resolution {
+    Root Meaning of the three terms (This definition is inspired by the similarity of their root lower resolutions.) {
+      The nature of Information means it is a System and is a Soul.
+      
+    }
+    Intention of Resolution { The intention of being at a lower resolution is to highlight the sameness of these terms. }
+    At higher Conceptual Resolutions { Information, System, & Soul have nuanced & different meanings. }
+  }
+  Existence/God {
+    Everything that "is", or more simply, everything (Physical, Conceptual, Social, Spirit). The overarching System.
+    Entity {
+      God is an Entity.    
+    
+    }
+  }
+  Information/Entity/System/Being/Soul/Culture/Philosophy/Religion {
+    That which can distinguish one thing from another.
+    Information which exhibits intelligence.
+    Entity { "http://en.wikipedia.org/wiki/Entity" }
+    System {
+      Physical System { "http://en.wikipedia.org/wiki/Physical_system" }
+      Conceptual System { "http://en.wikipedia.org/wiki/Conceptual_system" }
+    }
+    Soul { "http://en.wikipedia.org/wiki/Soul"
+      A Soul is a Being.
+      Animism { "http://en.wikipedia.org/wiki/Animism"
+        I favor Animism''s view of Soul.
+        It is defined by primitive humans, had an undistilled perspective.
+        Their view of souls recognizes the nature of intelligence in it''s primitive forms.
+        Example { Non-biological entities (such as rivers and mountains) possess souls. }
+        Cultural Ego {
+          My view is the Ourstory of Animism is: {
+            Animism is an earlier state of common worldview that has been opposed contemporary culture. {
+              The 1819 view of culture with the Amimism philosophy 
+              
+            }
+          
+          }
+        }
+      }
+    }
+    Being { "http://en.wikipedia.org/wiki/Being" }
+  }
+  Evolution {
+    The Iteration of design of an Entity based on feedback from Reality.
+    This Iteration can happen over Time or in a progression of steps.
+    If you think of Time as a progression of steps, like film, then the progression is the same.
+  }
+  Higher Being {
+    A Being that is more Evolved than another Being.
+    Evolved {
+      Progressed and closer to God in a Systemic sense. { The System has a variable Resolution. }
+    
+    }
+  }
+  Intelligence {
+    The cause/effect system of the Soul.
+    It also include control logic, Information transfer, Evolution.
+    Human-like Intelligence {
+      logic, abstract thought, understanding, self-awareness, communication, learning, emotional knowledge, memory, planning, and problem solving.
+      
+    }
+  }
+  State {
+    The status of Information.
+      
+  }
+  Time { "http://en.wikipedia.org/wiki/Time"
+    The sequence of States in Reality.
+    See "https://www.youtube.com/watch?v=p4Gotl9vRGs"
+  }
+  Pattern {
+    The elements of a pattern repeat in a predictable or regular manner.
+    Any of the five traditionally accepted senses may directly observe patterns.
+    Abstract patterns in science, spirit, mathematics, or language require conceptual & extrasensory senses.
+  }
+  Direct Observation {
+    Observation by a person of a System or Event from one or more of their senses.
+    Socially Accepted Senses {
+      A Collective''s definition of Direct Observation, is Observation using one or more Socially Accepted Senses.
+      Socially Accepted Senses differ according to the Perspective of the Reality it is in.
+    }
+    These observations are widespread in nature and in art.
+  }
+  Energy {
+    Energy is a property of objects, transferable among them via fundamental interactions, which can be converted in form but not created or destroyed.
+    The ability of a system (physical/conceptual) to do work on other systems (physical/conceptual)
+  }
+  Spirit {
+    The motivation of the soul in it''s current state.
+    The Holy Spirit causes people to vibrate at a higher level and perform actions with increased zeal.
+    The Spirit of Greed causes one to act in greedy ways.
+  }
+  Reality {
+    Plural { Realities }
+    The interpretation of existence from a soul''s perspective. The Information may be Consciously or Unconsciously experienced.
+    Reality can be seen as being singular (absolute), or being plural (relative).
+    e.g. A rock does not have a nervous system or much of a dynamic intelligence. It''s reality is more singular  
+    Reality can be socially defined 
+    Can be used to enforce one''s reality to another.
+    Often user to refer to a, The state of things as they actually exist, rather than as they may appear or might be imagined
+    Singular Reality {
+      An absolute view of Existence. From the perspective, Existence cannot be any other way.
+      
+    }
+    Plural Reality {
+      A Reality that accepts the existence of and can incorporate other Realities.
+      
+    }
+    Objective Reality {
+      Reality from the Perspective of a Higher Being.
 
-    Now as an experiment, view the fruit from an angle and ignore your memory of picking up the fruit & viewing it from other angles. Experience your perspective of the fruit. What sensations do you feel? Do you feel emotions? Is there a back story behind your sensations? Be aware of the backstory, if there is one. Understand the backstory while remaining detached from it.
-
-    You have just exercised having multiple perspectives on a single object or concept. This brings us to the magical incantation of words.
-
-    The Wizard {
-      Picture this. In an ancient white tower, a wizard in white is hunched over candlelight studying texts of arcana. Since childhood, he has studied the arts of magic. He pauses to remember when such studying came to fruition. When he saved the world from dominion of evil.
-
-      On a gloomy autumn day, a dark wizard opened a portal to the realms of hell. Beyond the gate, lies the dark lord, who controls every aspect of that realm to his pleasure. The subjects toil day after day under the eyes of the taskmasters. Any deviance from the strict procedures means being stripped of freedom to live a meaningful life and then becoming indoctrinated to themselves become taskmasters. The taskmasters'' only stimulation comes from stripping away the power & freedom of their subjects. However, such pleasure is short lived as their thirst for imprisoning others is insatiable.
-
-      In the dark world, there is a saying, "You have the freedom to do what you want to do". Everyone has "freedom". Everyone can make their own decisions. Nothing impedes their free will.
-
-      Disobedience is punished with the "responsibility" to hinder yourself & others from performing life''s purposes. Failure to live up to such responsibility degrades the quality of life of the individual & imposes more "responsibility" to impede life purposes.
-
-      Now the wizard in white always saw the danger of this dark world. He toiled to bring awareness to the population against the allure of the dark world. This allure attracted mistakes, most often coming from ignorance. If he could only make the population make the "right" decisions this danger would not exist. If the population were acting perfectly, then life would be perfect. He even had contingencies against those who disobeyed. He would show them the errors of their ways and they would then educate others against the dangers of acting against the divine plan.
-
-      The White Wizard implemented his plan to lead the population towards enlightenment. Some people were called by the White Wizard & chose to follow his lead. The White Wizard spoke directly to each person through thoughts, feelings, circumstances, words, visions. The called spoke to their brethren about their experiences. Groups of people with the same spirit also merged to become souls.
-
-      He began to notice that certain spiritual personalities began to teach their personal message to the population. The devotees loved hearing the personal messages as it resonated with them. When the strong personalities passed on, the devotees created institutions to preserve & propagate the teachings of their masters. These institutions tried their best to instill the spirit & teachings of their masters. However, an institution is a life form separate from the spiritual master. Even spiritual masters are different lifeforms from themselves as they pass through life. For a soul travels on the road of time, yet evolves.
-
+    }
+    Being Aware of other Realities has the quality to allows a Soul to live within that particular Reality.
+  }
+  Perspective/Abstraction {
+    The view of reality from a soul''s point of view
+    Abstraction {
+      The view of reality from a Model''s point of view
+      Characteristic Traits {
+        Abstraction is:
+        * to take a distance from the physical world
+        * (or is a close to) generalization
+        * information hiding
+        * to keep only relevant aspects
+        * a kind of reformulation or approximation
+        * a to change levels of detail
+      }
+      Model { The Characteristic Traits of abstraction can then be used to build up a Model. }
+      Link { "http://www.aaai.org/ocs/index.php/SARA/SARA13/paper/download/7249/6267" }
+    }
+  }
+  Resolution {
+    The detail & scope of the perspective.
+    Detail is increased with iteration & meditation.
+    A broad perspective on a system usually means a low resolution to be consciously aware of the information.
+    One can focus perspective to gain a higher resolution.
+    You can zoom in & out of the system to observe details & patterns.
+    Fractal {
+      Resolution is increased when zooming into a Fractal.
+      Resolution is decreased when zooming out of a Fractal.
+    }
+    First Order Resolution {
+      Does not include another Resolution.
+      
+    }
+    Second Order Resolution {
+      A Resolution that consists of multiple Resolutions.
+      Often not distinguished by a Resolution, thus seen as a property of a common Resolution. Like a path through Resolutions.
+    }
+    Scale Confusion {
+      A skewed projection of an Object/Concept in a different Resolution based on the current Resolution.
 
     }
   }
+  Story/Ourstory/History/Herstory {
+    Recounting of a sequence of events/Narrative {
+      A Story is algorithmic.
+      It is often of a low resolution, thus requiring little information to communicate (or transfer) a Conceptual System.
+      It can be iterated upon to create more resolution.
+    }
+  }
+  Tool {
+    Conceptual or Physical System that is used to create a desired effect on another System or itself.
+    
+  }
+  Technology {
+    Quote { "The Extensions of Man", "Marshall McLuhan" }
+
+  }
+  Fractal {
+    A curve or geometric figure, each part of which has the same statistical character as the whole.
+    Fractals are useful in modeling structures (such as eroded coastlines or snowflakes) in which similar patterns: {
+      Recur at progressively smaller scales
+      In describing partly random or chaotic phenomena such as: {
+        Crystal growth
+        Fluid turbulence
+        Galaxy formation
+      }
+    }
+  }
+  Rules {
+    
+    
+  }
+  Idolatry { "http://en.wikipedia.org/wiki/Idolatry"
+    The definition of God as an Entity less than (a part of) Existence.
+    
+  }
+  Oneness {
+    The initial State of Existence.
+    We used to all be one Being and Fully Conscious of it.
+    The Physical expression of Oneness is The Singularity?
+  }
+  Fully Conscious {
+    Fully experiencing the full State of a Being.
+    Right at this moment, I am a Fully Conscious version of Brian Takita''s present Consciousness.
+    Often used to relate to Enlightenment. Fully Conscious with a Higher Form.
+  }
+  Concrescence {
+    A local state of unusually high complexity.
+
+  }
 }
-
-The Holy Grail {
-  Inner creation of your life. "You and the land are one".
-
+Notes {
+  All human conceptual experience into one cohesive whole.
+  Technology, Culture, & Spiritual
+  These days, our fundamental notions of existence, God is Evolves over Time.
 }
